77d58ae95d62868682e31ad798e02168f3544b73
eLeGarage <131270758+lesimpleliott@users.noreply.github.com>
2024-02-17 13:52:14 +0100

    about page

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,10 +2,10 @@ import PropTypes from "prop-types";
 
 const Banner = (props) => {
     return (
-        <div className="heroBanner">
+        <section className="heroBanner">
             <img src={props.bannerData.img} alt="image hero banner" />
             <p>{props.bannerData.text}</p>
-        </div>
+        </section>
     );
 };
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,17 +1,32 @@
 import Banner from "../components/Banner";
+import CollapseMenu from "../components/CollapseMenu";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 const bannerData = {
-    img: "src/assets/aboutImage.jpg"
+    img: "src/assets/aboutImage.jpg",
 };
 
+import dataAbout from "../data/about.json";
+
 const About = () => {
+
+
     return (
         <div>
             <Header />
             <Banner bannerData={bannerData} />
-            ABOUT
+
+            <section className="aboutSection">
+                {dataAbout.map((item) => (
+                    <CollapseMenu
+                        key={item.id}
+                        title={item.title}
+                        description={item.description}
+                    />
+                ))}
+            </section>
+
             <Footer />
         </div>
     );
