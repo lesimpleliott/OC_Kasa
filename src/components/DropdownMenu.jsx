62bfea1b853899bea0cb7111fac3aa40440ae045
eLeGarage <131270758+lesimpleliott@users.noreply.github.com>
2024-02-20 16:49:12 +0100

    V1 almost done

--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -1,31 +1,31 @@
-import PropTypes from "prop-types";
-import { useState } from "react";
+// import PropTypes from "prop-types";
+// import { useState } from "react";
 
-const DropdownMenu = ({ item }) => {
-    const [menuIsOpen, setMenuIsOpen] = useState(false);
+// const DropdownMenu = ({ item }) => {
+//     const [menuIsOpen, setMenuIsOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setMenuIsOpen((prevMenuIsOpen) => !prevMenuIsOpen);
-    };
+//     const toggleMenu = () => {
+//         setMenuIsOpen((prevMenuIsOpen) => !prevMenuIsOpen);
+//     };
 
-    return (
-        <div className="menu">
-            <div className="menu__banner" onClick={toggleMenu}>
-                <h3>{item.title}</h3>
-                <i className={`fa-solid fa-chevron-down ${menuIsOpen ? 'open' : ''}`}></i>
-            </div>
-            <div className={`menu__content ${menuIsOpen ? 'open' : ''}`}>
-                <p>{item.description}</p>
-            </div>
-        </div>
-    );
-};
+//     return (
+//         <div className="menu">
+//             <div className="menu__banner" onClick={toggleMenu}>
+//                 <h3>{item.title}</h3>
+//                 <i className={`fa-solid fa-chevron-down ${menuIsOpen ? 'open' : ''}`}></i>
+//             </div>
+//             <div className={`menu__content ${menuIsOpen ? 'open' : ''}`}>
+//                 <p>{item.description}</p>
+//             </div>
+//         </div>
+//     );
+// };
 
-DropdownMenu.propTypes = {
-    item: PropTypes.shape({
-        title: PropTypes.string.isRequired,
-        description: PropTypes.string.isRequired,
-    }).isRequired,
-};
+// DropdownMenu.propTypes = {
+//     item: PropTypes.shape({
+//         title: PropTypes.string.isRequired,
+//         description: PropTypes.string.isRequired,
+//     }).isRequired,
+// };
 
-export default DropdownMenu;
+// export default DropdownMenu;
--- src/pages/Host.jsx
+++ src/pages/Host.jsx
@@ -5,6 +5,7 @@ import dataHosts from "../data/base.json";
 import { useState, useEffect } from "react";
 import Rating from "../components/Rating";
 import Carousel from "../components/Carousel";
+import DropdownMenu from "../components/DropdownMenu";
 
 const Host = () => {
     const { id } = useParams();
@@ -21,37 +22,38 @@ const Host = () => {
 
             {hostData && (
                 <main className="host">
-                   
                     <Carousel pictures={hostData.pictures} />
 
-
                     <section className="content">
-                        <h2 className="title">{hostData.title}</h2>
-                        <p className="city">{hostData.location}</p>
-                        <div className="tags">
-                            {hostData.tags.map((tag, index) => (
-                                <span className="tag" key={index}>
-                                    {tag}
-                                </span>
-                            ))}
+                        <div className="content__host">
+                            <h2 className="title">{hostData.title}</h2>
+                            <p className="city">{hostData.location}</p>
+                            <div className="tags">
+                                {hostData.tags.map((tag, index) => (
+                                    <span className="tag" key={index}>
+                                        {tag}
+                                    </span>
+                                ))}
+                            </div>
                         </div>
 
-                        <div className="profil">
-                            <p className="profil__name">{hostData.host.name}</p>
-                            <img
-                                src={hostData.host.picture}
-                                alt=""
-                                className="profil__img"
-                            />
+                        <div className="content__owner">
+                            <div className="detail">
+                                <p className="name">{hostData.host.name}</p>
+                                <img src={hostData.host.picture} />
+                            </div>
+                            <Rating rating={parseInt(hostData.rating)} />
                         </div>
-
-                        <Rating rating={parseInt(hostData.rating)}/>
-                                
                     </section>
 
-                    <section className="description"></section>
+                    <section className="description">
+                    <DropdownMenu item={"test"} />
 
-                    {/* <h2>{hostData ? hostData.title : ""}</h2> */}
+                        <DropdownMenu title="Titre du Dropdown">
+                            <p>Contenu du dropdown ici</p>
+                            <p>contenu suite</p>
+                        </DropdownMenu>
+                    </section>
                 </main>
             )}
 
